5dfb54c1c5f5ae9bc7c3cc5cd98fd0e071396c09
Мария Мокрый <masha.mokryy@mail.ru>
2022-09-27 12:22:47 +0800

    added function in index.ts

--- index.js
+++ index.js
@@ -14,101 +14,12 @@ difference %= 3600;
 minutes = Math.floor(difference / 60);
 seconds = difference % 60;
 
-ans += days;
-switch (days % 10) {
-    case 1: {
-        ans += " день ";
-        break;
-    }
-    case 2:
-    case 3:
-    case 4: {
-        ans += " дня ";
-        break;
-    }
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-    case 0: {
-        ans += " дней ";
-        break;
-    }
-}
-
-
-
-ans += hours;
-switch (hours % 10) {
-    case 1: {
-        ans += " час ";
-        break;
-    }
-    case 2:
-    case 3:
-    case 4: {
-        ans += " часа ";
-        break;
-    }
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-    case 0: {
-        ans += " часов ";
-        break;
-    }
-}
-
-
-
-ans += minutes;
-switch (minutes % 10) {
-    case 1: {
-        ans += " минута ";
-        break;
-    }
-    case 2:
-    case 3:
-    case 4: {
-        ans += " минуты ";
-        break;
-    }
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-    case 0: {
-        ans += " минут ";
-        break;
-    }
-}
-
-
-ans += seconds;
-switch (seconds % 10) {
-    case 1: {
-        ans += " секунда.";
-        break;
-    }
-    case 2:
-    case 3:
-    case 4: {
-        ans += " секунды.";
-        break;
-    }
-    case 5:
-    case 6:
-    case 7:
-    case 8:
-    case 9:
-    case 0: {
-        ans += " секунд.";
-        break;
-    }
-}
+ans += days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds.";
 
 console.log(ans);
+
+
+
+
+
+
